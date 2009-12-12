4717029fe7fabfdc903d0a169366a5e1b05791f8
voidy21 <y2.void@gmail.com>
2009-12-13 00:31:27 +0900

    整形した

--- src/googlereader/Entries.java
+++ src/googlereader/Entries.java
@@ -9,29 +9,30 @@ import org.w3c.dom.Node;
  * @author voidy21
  */
 public class Entries {
+
   String title;
   String updated;
   List<Feed> feedList = new ArrayList<Feed>();
-  public Entries(Node root) {
-     int len = root.getChildNodes().getLength();
-     for(int i=0;i<len;i++){
-       Node node = root.getChildNodes().item(i);
-       if(node.hasChildNodes()){
-         String nodeName = node.getNodeName();
-         if(nodeName.equals("title")){
-           title = node.getFirstChild().getNodeValue();
-         }
-         if(nodeName.equals("updated")){
-           updated = node.getFirstChild().getNodeValue();
-         }
-         if(nodeName.equals("entry")){
-           Feed feed = new Feed(node);
-           feedList.add(feed);
-         }
-       }
-     }
-     System.out.println(title);
-     System.out.println(updated);
-  }
 
+  public Entries(Node root) {
+    int len = root.getChildNodes().getLength();
+    for ( int i = 0; i < len; i++ ) {
+      Node node = root.getChildNodes().item(i);
+      if ( node.hasChildNodes() ) {
+        String nodeName = node.getNodeName();
+        if ( nodeName.equals("title") ) {
+          title = node.getFirstChild().getNodeValue();
+        }
+        if ( nodeName.equals("updated") ) {
+          updated = node.getFirstChild().getNodeValue();
+        }
+        if ( nodeName.equals("entry") ) {
+          Feed feed = new Feed(node);
+          feedList.add(feed);
+        }
+      }
+    }
+    System.out.println(title);
+    System.out.println(updated);
+  }
 }
